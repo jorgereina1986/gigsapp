d9f1b67502f3f731e087ca2d368d3260dfb081f6
jorgereina1986 <jorgereina1986@gmail.com>
2017-01-27 06:40:51 -0500

    edited main activity

--- app/src/main/java/com/jorgereina/gigs/MainActivity.java
+++ app/src/main/java/com/jorgereina/gigs/MainActivity.java
@@ -2,9 +2,7 @@ package com.jorgereina.gigs;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,7 +10,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -38,10 +35,6 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initViews();
-
-
-        String username = usernameEt.getText().toString();
-        String password = passwordEt.getText().toString();
 
 
         FirebaseDatabase fdb = FirebaseDatabase.getInstance();
@@ -75,7 +68,7 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_logout) {
             return true;
         }
 
@@ -110,14 +103,6 @@ public class MainActivity extends AppCompatActivity
     public void initViews(){
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -128,10 +113,6 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        usernameEt = (EditText) findViewById(R.id.username_et);
-        passwordEt = (EditText) findViewById(R.id.password_et);
-        registerBtn = (Button) findViewById(R.id.register_btn);
-        signInBtn = (Button) findViewById(R.id.sign_in_btn);
     }
 
 }
