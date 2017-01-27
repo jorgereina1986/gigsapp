1d80120d4a214e5eed2bb0c76b99f94a9b0c0d1b
jorgereina1986 <jorgereina1986@gmail.com>
2017-01-27 07:41:12 -0500

    creating user and adding to database

--- app/src/main/java/com/jorgereina/gigs/SignUpActivity.java
+++ app/src/main/java/com/jorgereina/gigs/SignUpActivity.java
@@ -1,13 +1,79 @@
 package com.jorgereina.gigs;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    protected EditText passwordEditText;
+    protected EditText emailEditText;
+    protected Button signUpButton;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //init firebase
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        passwordEditText = (EditText) findViewById(R.id.sign_up_password_field);
+        emailEditText = (EditText) findViewById(R.id.sign_up_email_field);
+        signUpButton = (Button) findViewById(R.id.sign_up_button);
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String email = emailEditText.getText().toString();
+                String password =  passwordEditText.getText().toString();
+                email = email.trim();
+                password = password.trim();
+
+                if (password.isEmpty() || email.isEmpty()){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+                    builder.setMessage(R.string.login_error_message)
+                            .setTitle(R.string.login_error_title)
+                            .setPositiveButton(android.R.string.ok, null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+                else {
+                    firebaseAuth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()){
+                                        Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                        startActivity(intent);
+                                    }
+                                    else {
+                                        AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+                                        builder.setMessage(task.getException().getMessage())
+                                                .setTitle(R.string.login_error_title)
+                                                .setPositiveButton(android.R.string.ok, null);
+                                        AlertDialog dialog = builder.create();
+                                        dialog.show();
+                                    }
+                                }
+                            });
+                }
+            }
+        });
     }
 }
